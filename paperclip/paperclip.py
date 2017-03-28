8a275f38c2c9a114043e706c343d84e83e6d58de
Кречетов Максим Валентинович <m.krechetov@e2e4online.ru>
2017-03-28 12:33:27 +0700

    Paperclip new tz

--- paperclip/paperclip.py
+++ paperclip/paperclip.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask import send_file
-from flask import abort
 from paperclip.app.image_processing import ImageProcessor
-from pathlib import Path
 import config
 
 app = Flask(__name__)
@@ -12,27 +10,7 @@ app.config['STORE_DIR'] = config.STORE_DIR
 
 
 @app.route('/<path:path>')
-def process(path):
-    full_path = app.config['STORE_DIR'] + path
-
-    # Проверка на верный URL (наличие расширения)
-    try:
-        ext = path.split('.')[1]
-        mimetype = 'image/' + ext
-    except Exception as e:
-        print(e)
-        abort(404)
-
-    image_path = Path(full_path)
-
-    # Проверяем файл на наличие
-    if image_path.is_file():
-        return send_file(full_path, mimetype=mimetype)
-
-    try:
-        img = ImageProcessor(path)
-        img.process()
-        return send_file(full_path, mimetype=mimetype)
-    except Exception as e:
-        print(e)
-        abort(404)
+def magic(path):
+    processor = ImageProcessor(path)
+    processor.process()
+    return send_file(processor.get_full_path(), mimetype=processor.get_mimetype())
