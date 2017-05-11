078118a7396ee58d55cc662015eda9bf6487fb77
Кречетов Максим Валентинович <m.krechetov@e2e4online.ru>
2017-05-11 10:18:59 +0700

    cfg

--- paperclip/paperclip.py
+++ paperclip/paperclip.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import send_file
+from werkzeug.contrib.fixers import ProxyFix
 from paperclip.app.image_processing import ImageProcessor
 import config
 
@@ -14,3 +15,7 @@ def magic(path):
     processor = ImageProcessor(path)
     processor.process()
     return send_file(processor.get_full_path(), mimetype=processor.get_mimetype())
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+if __name__ == '__main__':
+    app.run()
